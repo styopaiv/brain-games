761c031b2cb1cc87e03257b6d8d79c414397b513
xibalbaroad <aastralplane@gmail.com>
2017-08-10 18:27:17 +1100

    added game rules

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -3,6 +3,8 @@
 import readlineSync from 'readline-sync';
 import { askName, welcomeUser } from '..';
 
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".\n');
 const name = askName();
 
 welcomeUser(name);
